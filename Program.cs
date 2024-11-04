845ea6ccffcd3be0ef9debfb4799b4f0ffcaab43
Владислав Микитчук <Mykytchuk_Vl@ifnmu.edu.ua>
2024-11-04 14:43:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    private static Mutex mutex = new Mutex();
+    private static int sharedResource = 0;
+
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Thread[] threads = new Thread[5];
+        for (int i = 0; i < threads.Length; i++)
+        {
+            threads[i] = new Thread(AccessResource);
+            threads[i].Name = $"Thread {i + 1}";
+            threads[i].Start();
+        }
+
+        
+        foreach (Thread t in threads)
+        {
+            t.Join();
+        }
+
+        Console.WriteLine("Усі потоки завершено.");
+    }
+
+    
+    static void AccessResource()
+    {
+        Console.WriteLine($"{Thread.CurrentThread.Name} очікує доступ до ресурсу.");
+
+        
+        mutex.WaitOne();
+
+        try
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} отримав доступ до ресурсу.");
+            sharedResource++;
+            Console.WriteLine($"{Thread.CurrentThread.Name} змінив ресурс: {sharedResource}");
+            Thread.Sleep(1000); 
+        }
+        finally
+        {
+            
+            Console.WriteLine($"{Thread.CurrentThread.Name} звільнив ресурс.");
+            mutex.ReleaseMutex();
+        }
+    }
+}
